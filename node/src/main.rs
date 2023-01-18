2ff99aa8ebf27fee77bd518df4c73909b5bec9aa
Akhil <akhilsai2712@gmail.com>
2023-01-18 17:02:24 -0500

    added config changes

--- node/src/main.rs
+++ node/src/main.rs
@@ -56,10 +56,10 @@ async fn main() -> Result<()> {
     let exit_tx;
     match vss_type{
         "ped" =>{
-            exit_tx = pedavss_cc::node::Context::spawn(config,sleep.try_into().unwrap()).unwrap();
+            exit_tx = pedavss_cc::node::Context::spawn(config,sleep).unwrap();
         },
         "fre" => {
-            exit_tx = hash_cc::node::Context::spawn(config,sleep.try_into().unwrap()).unwrap();
+            exit_tx = hash_cc::node::Context::spawn(config,sleep).unwrap();
         },
         "hr" => {
             exit_tx = hash_cc_baa::node::Context::spawn(config,sleep,batch).unwrap();
